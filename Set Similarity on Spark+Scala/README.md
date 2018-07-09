28e26eb0f8faa526887908d087f6e7b3b9010afd
daniel <daniel11zhang@gmail.com>
2018-07-09 13:33:41 +1000

    V1.0
    Guarantee similarity only computing once for same pairs

--- Set Similarity on Spark+Scala/README.md
+++ Set Similarity on Spark+Scala/README.md
@@ -1,6 +1,6 @@
 # Set Similarity Join Using Spark+Scala on AWS
 
-Given a threshold, the program outputs all pairs of record with similarity greater or equal to threshold.
+Given a threshold, the program outputs all pairs of record with similarity greater or equal to the threshold.
 
 ### Jaccard similarity:
 `sim(C1, C2) = |C1∩C2| / |C1∪C2|`
@@ -9,6 +9,14 @@ Given a threshold, the program outputs all pairs of record with similarity great
 `inputFile outputFolder threshold`
 
 ### Format of Input Files:
-`index element1 element2 element...`
+| Index   |      Attributes |
+|----------|:-------------:|
+|0|today is a good day|
+|1|good morning|
+|2|have a nice day|
+|...|....|
 
-### Optimize the performance with PPjoin and Transformations selection
+### Optimizations
+* PPjoin
+* Similarity Will Only Be Computed Once For Same Pairs
+* Using Equivalent Transformations But had Higher Performance
--- Set Similarity on Spark+Scala/SetSimJoin.scala
+++ Set Similarity on Spark+Scala/SetSimJoin.scala
@@ -5,58 +5,70 @@ import org.apache.spark.SparkConf
 object SetSimJoin {
   def main(args: Array[String]) {
 
-    //input parameters
+    //helper functions
+    def prefixLen(x:Int, thres:Double):Int = x - (x * thres).ceil.toInt + 1     //calculate prefix length
+
+    def overlap(x:Array[Int], y:Array[Int], thres:Double):Double =              //calculate min number of common elements required. handle precision problem.
+      (math rint (thres/(1+thres)) * (x.length+y.length) * 100) /100
+
+    def sim(x:Array[Int], y:Array[Int], key:Int):Double = {                     //compute jaccard similarity. Guarantee only computing once for same pairs
+      var (i, j, intersect, firstComIsKey) = (0, 0, 0.0, false);
+      while(i < x.length && j < y.length){
+        if(x(i) > y(j)) j+=1;
+        else if(x(i) < y(j)) i+=1;
+        else{
+          if(!firstComIsKey){
+            if(x(i) != key) return -1.0;
+            firstComIsKey = true;
+          }
+          intersect += 1.0;
+          i+=1;
+          j+=1;
+        }
+      }
+      return intersect / (x.length + y.length - intersect)
+    }
+
+    //input parameters & configuration
     val inputFile = args(0)
     val outputFolder = args(1)
     val threshold = args(2).toDouble
-
-    //configuration
     val conf = new SparkConf().setAppName("SetSimJoin")
     val sc = new SparkContext(conf)
 
-    def prefixLen(x:Int):Int = x - (x * threshold).ceil.toInt + 1                 //prefix length
-
-    def overlap(x:Array[Int], y:Array[Int]):Double =                              //min number of common elements required
-      (math rint (threshold/(1+threshold)) * (x.length+y.length) * 100) /100
-
-    def sim(x:Array[Int], y:Array[Int]):Double = {                                //jaccard similarity
-      val intersectLen = x.intersect(y).length
-      intersectLen.toDouble / (x.length + y.length - intersectLen)
-    }
-
-    //main computation
-    val record = sc.textFile(inputFile).mapPartitions(                             //turn input data to (rid, [attributes]) pairs, and cache them
+    //spark transformations and actions
+    val record = sc.textFile(inputFile).mapPartitions(                          //turn input data to (rid, [attributes]) pairs, then cache it
                   _.map( _.split(" ").map(_.toInt) )
                 ).cache()
 
-    val G = sc.broadcast(                                                          //sort tokens in ascending order by frequency, then broadcast it
+    val G = sc.broadcast(                                                       //sort tokens in ascending order by frequency, then broadcast it
             record.flatMap( _.drop(1).map((_,1)) ).
             reduceByKey(_+_).sortBy(_._2).
             keys.collect.zipWithIndex.toMap
           )
 
     val result = record.flatMap{x=>
-            val sorted = x.drop(1).map(G.value(_)).sorted;                         //convert each attribute to its corresponding index in G, and sort it
-            for(i<- 0 until prefixLen(sorted.length) )
-              yield ( sorted(i), Array((sorted.length-i-1, x(0), sorted)) )        //replicate a pair of record for each prefix
+            val sorted = x.drop(1).map(G.value(_)).sorted;                      //convert each attribute to its corresponding index in G, then sort it
+            for(i<- 0 until prefixLen(sorted.length, threshold) )
+              yield ( sorted(i), Array((sorted.length-i-1, x(0), sorted)) )     //replicate a pair of record for each prefix
 
           }.reduceByKey(_++_).
-          filter(_._2.length > 1).                                                 //drop those sample no more than 2
+          filter(_._2.length > 1).                                              //drop those sample no more than 2
           flatMap{ x=>
             val arr = x._2;
             val similarity = for{
               i<- 0 until arr.length
               j<- i+1 until arr.length
               min_len = arr(i)._3.length.min(arr(j)._3.length)
-              if(min_len >= min_len*threshold)                                     //length filter
-              if(1 + arr(i)._1.min(arr(j)._1)  >= overlap(arr(i)._3, arr(j)._3))   //positional filter
-            } yield ((arr(i)._2, arr(j)._2), sim(arr(i)._3, arr(j)._3));           //compute jaccard similarity for each pairs
+              if(min_len >= min_len*threshold)                                  //length filter
+              if(1 + arr(i)._1.min(arr(j)._1)  >= overlap(arr(i)._3, arr(j)._3, threshold))   //positional filter
+            } yield ((arr(i)._2, arr(j)._2), sim(arr(i)._3, arr(j)._3, x._1));  //compute jaccard similarity for each pairs
 
-            similarity.filter(_._2>=threshold)                                     //keep those pairs of rid with sim >= threshold
+            similarity.filter(_._2>=threshold)                                  //keep those pairs of rid with sim >= threshold
 
-          }.distinct.                                                              //drop duplicates
+          }.distinct.                                                           //drop duplicates
           sortByKey().
-          mapPartitions( _.map(x => x._1 + "\t" + x._2) )                          //format the result
+          mapPartitions( _.map(x => x._1 + "\t" + x._2) )                       //format the result
 
     result.saveAsTextFile(outputFolder)
   }
